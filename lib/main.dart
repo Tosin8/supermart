ca03a95fe21f6027b274223d77349b31710267d6
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-07-19 20:57:39 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,6 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-
-//
-import 'package:flutter/material.dart';
-import 'package:shop_app/routes.dart';
-import 'package:shop_app/screens/profile/profile_screen.dart';
-import 'package:shop_app/screens/splash/splash_screen.dart';
-import 'package:shop_app/theme.dart';
-
-void main() {
   runApp(MyApp());
 }
 
@@ -39,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Super Mart',
       theme: theme(),
       // home: SplashScreen(),
       // We use routeName so that we dont need to remember the name
